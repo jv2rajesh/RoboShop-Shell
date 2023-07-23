4a0ed54f2b0497ebb58a16fcd8ffb0ac15d13418
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 12:37:34 +0530

    added echo in catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,40 @@
 echo -e  "\e[36m>>>>>>>> create catalogue service <<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> create mongo repo <<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> install nodejs repos <<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> Install nodejs <<<<<<<<<<\e[0m"
-yum install nodejs -y
+yum install nodejs -y >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> creating application user <<<<<<<<<<\e[0m"
-useradd roboshop
+useradd roboshop >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> removing existing content in add directory <<<<<<<<<<\e[0m"
-rm -rf /app
+rm -rf /app >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> creating directory <<<<<<<<<<\e[0m"
-mkdir /app
+mkdir /app >/tmp/roboshop.log >/tmp/roboshop.log
 echo -e  "\e[36m>>>>>>>> download application content <<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> extracting application content <<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip >/tmp/roboshop.log
 cd /app
 
 echo -e  "\e[36m>>>>>>>> installing Nodejs dependencies <<<<<<<<<<\e[0m"
-npm install
+npm install >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> installing mongodb client <<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> adding catalogue schema <<<<<<<<<<\e[0m"
 
-mongo --host mongodb.jv2rajesh.online </app/schema/catalogue.js
+mongo --host mongodb.jv2rajesh.online </app/schema/catalogue.js >/tmp/roboshop.log
 
 echo -e  "\e[36m>>>>>>>> start catalogue service <<<<<<<<<<\e[0m"
 
