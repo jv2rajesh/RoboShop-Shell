90c3ead858fa66371f85de0516fb131f9eadd0da
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 15:31:53 +0530

    added echo in catalogue

--- redis.sh
+++ redis.sh
@@ -1,9 +1,11 @@
 echo -e "\e[36m>>>>>>>> Installing repo<<<<<<<<\e[om"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
 echo -e "\e[36m>>>>>>>>Enable Redis 6.2 from package streams <<<<<<<<\e[om"
 yum module enable redis:remi-6.2 -y
 
 echo -e "\e[36m>>>>>>>> Installing redis <<<<<<<<\e[om"
+
 yum install redis -y
 # Update listen address
 systemctl enable redis
