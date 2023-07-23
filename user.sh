f3820f9e2192ff80c5437c6a4f9158591f75316b
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 20:21:02 +0530

    updated user from catalogue

--- user.sh
+++ user.sh
@@ -1,20 +1,48 @@
 
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+log=/tmp/roboshop.log
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+echo -e  "\e[36m>>>>>>>> create user service <<<<<<<<<<\e[0m"
+cp user.service /etc/systemd/system/user.service &>>${log}
+
+echo -e  "\e[36m>>>>>>>> create mongo repo <<<<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+
+echo -e  "\e[36m>>>>>>>> install nodejs repos <<<<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+
+echo -e  "\e[36m>>>>>>>> Install nodejs <<<<<<<<<<\e[0m"
+yum install nodejs -y &>>${log}
+
+echo -e  "\e[36m>>>>>>>> creating application user <<<<<<<<<<\e[0m"
+useradd roboshop &>>${log}
+
+echo -e  "\e[36m>>>>>>>> removing existing content in add directory <<<<<<<<<<\e[0m"
+rm -rf /app &>>${log}
+
+echo -e  "\e[36m>>>>>>>> creating directory <<<<<<<<<<\e[0m"
+mkdir /app &>>${log} &>>${log}
+echo -e  "\e[36m>>>>>>>> download application content <<<<<<<<<<\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log}
+
+echo -e  "\e[36m>>>>>>>> extracting application content <<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/user.zip
+unzip /tmp/user.zip &>>${log}
 cd /app
-npm install
 
-yum install mongodb-org-shell -y
+echo -e  "\e[36m>>>>>>>> installing Nodejs dependencies <<<<<<<<<<\e[0m"
+npm install &>>${log}
 
-mongo --host mongodb.jv2rajesh.online </app/schema/user.js
+echo -e  "\e[36m>>>>>>>> installing mongodb client <<<<<<<<<<\e[0m"
+yum install mongodb-org-shell -y &>>${log}
+
+echo -e  "\e[36m>>>>>>>> adding user schema <<<<<<<<<<\e[0m"
+
+mongo --host mongodb.jv2rajesh.online </app/schema/user.js &>>${log}
+
+echo -e  "\e[36m>>>>>>>> start user service <<<<<<<<<<\e[0m"
+
+
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
+
