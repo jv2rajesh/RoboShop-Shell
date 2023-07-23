91b9e4c36015051be9586fe98a00500c216471d9
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 20:18:12 +0530

    added echo in catalogue

--- user.sh
+++ user.sh
@@ -1,6 +1,8 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
 cp user.service /etc/systemd/system/user.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 mkdir /app
