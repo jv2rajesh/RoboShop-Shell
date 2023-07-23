1d56b6053a0afe3e99849ed4b628d170b82f9a72
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 11:59:03 +0530

    added echo in catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,38 @@
-echo ">>>>>>>> create catalogue service <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> create catalogue service <<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo ">>>>>>>> create mongo repo <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> create mongo repo <<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo ">>>>>>>> install nodejs repos <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> install nodejs repos <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>>>>> Install nodejs <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> Install nodejs <<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo ">>>>>>>> creating application user <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> creating application user <<<<<<<<<<\e[0m"
 useradd roboshop
-echo ">>>>>>>> creating directory <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> creating directory <<<<<<<<<<\e[0m"
 mkdir /app
-echo ">>>>>>>> download application content <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> download application content <<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo ">>>>>>>> extracting application content <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> extracting application content <<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo ">>>>>>>> installing Nodejs dependencies <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> installing Nodejs dependencies <<<<<<<<<<\e[0m"
 npm install
 
-echo ">>>>>>>> installing mongodb client <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> installing mongodb client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo ">>>>>>>> adding catalogue schema <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> adding catalogue schema <<<<<<<<<<\e[0m"
 
 mongo --host mongodb.jv2rajesh.online </app/schema/catalogue.js
 
-echo ">>>>>>>> start catalogue service <<<<<<<<<<"
+echo -e  "\e[36m>>>>>>>> start catalogue service <<<<<<<<<<\e[0m"
 
 systemctl daemon-reload
 systemctl enable catalogue
