825ba2e6ed5664549f5ae7c695ad4e76a0d75f0b
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 23:38:54 +0530

    added function

--- common.sh
+++ common.sh
@@ -2,8 +2,8 @@
 nodejs() {
 log=/tmp/roboshop.log
 
-echo -e  "\e[36m>>>>>>>> create user service <<<<<<<<<<\e[0m"
-cp user.service /etc/systemd/system/user.service &>>${log}
+echo -e  "\e[36m>>>>>>>> create ${component} service <<<<<<<<<<\e[0m"
+cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
 
 echo -e  "\e[36m>>>>>>>> create mongo repo <<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
@@ -14,8 +14,8 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 echo -e  "\e[36m>>>>>>>> Install nodejs <<<<<<<<<<\e[0m"
 yum install nodejs -y &>>${log}
 
-echo -e  "\e[36m>>>>>>>> creating application user <<<<<<<<<<\e[0m"
-useradd roboshop &>>${log}
+echo -e  "\e[36m>>>>>>>> creating application ${component} <<<<<<<<<<\e[0m"
+${component}add roboshop &>>${log}
 
 echo -e  "\e[36m>>>>>>>> removing existing content in add directory <<<<<<<<<<\e[0m"
 rm -rf /app &>>${log}
@@ -23,11 +23,11 @@ rm -rf /app &>>${log}
 echo -e  "\e[36m>>>>>>>> creating directory <<<<<<<<<<\e[0m"
 mkdir /app &>>${log} &>>${log}
 echo -e  "\e[36m>>>>>>>> download application content <<<<<<<<<<\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log}
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
 
 echo -e  "\e[36m>>>>>>>> extracting application content <<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/user.zip &>>${log}
+unzip /tmp/${component}.zip &>>${log}
 cd /app
 
 echo -e  "\e[36m>>>>>>>> installing Nodejs dependencies <<<<<<<<<<\e[0m"
@@ -36,15 +36,15 @@ npm install &>>${log}
 echo -e  "\e[36m>>>>>>>> installing mongodb client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y &>>${log}
 
-echo -e  "\e[36m>>>>>>>> adding user schema <<<<<<<<<<\e[0m"
+echo -e  "\e[36m>>>>>>>> adding ${component} schema <<<<<<<<<<\e[0m"
 
-mongo --host mongodb.jv2rajesh.online </app/schema/user.js &>>${log}
+mongo --host mongodb.jv2rajesh.online </app/schema/${component}.js &>>${log}
 
-echo -e  "\e[36m>>>>>>>> start user service <<<<<<<<<<\e[0m"
+echo -e  "\e[36m>>>>>>>> start ${component} service <<<<<<<<<<\e[0m"
 
 
 systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+systemctl enable ${component}
+systemctl restart ${component}
 }
 
